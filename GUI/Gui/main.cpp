bc9e772d96230e97dbd47abd666e0ebda986c5ab
Emma Barme <Emma@wpa068049.wireless.mcgill.ca>
2014-06-10 17:31:00 -0400

    Amelioration de la fenetre de demarrage, bouton quitter actif

--- GUI/Gui/main.cpp
+++ GUI/Gui/main.cpp
@@ -12,13 +12,37 @@ int main(int argc, char *argv[])
 
     //Creation d'une fenetre de demarrage qui permet de creer une nouvelle partie ou d'en charger une
     QWidget fenetreDemarrage;
-    fenetreDemarrage.setFixedSize(300, 150);
+    fenetreDemarrage.setFixedSize(900, 450);
 
+    //Police utilisée pour les textes et les boutons
+    QFont maPolice("Garamond", 14);
+
+    //Message de bienvenue
     QLabel *messageBienvenue = new QLabel(&fenetreDemarrage);
     messageBienvenue->setFrameStyle(QFrame::Panel | QFrame::Sunken);
     messageBienvenue->setText("Bienvenue");
     messageBienvenue->setAlignment(Qt::AlignHCenter);
-    messageBienvenue->setGeometry(100, 10, 100, 20);
+    messageBienvenue->setGeometry(300, 20, 300, 30);
+    messageBienvenue->setFont(QFont("Garamond", 18));
+
+    //Bouton pour créer une nouvelle partie
+    QPushButton *boutonNouvellePartie = new QPushButton("Créer une nouvelle partie", &fenetreDemarrage);
+    boutonNouvellePartie->setFont(maPolice);
+    boutonNouvellePartie->setCursor(Qt::PointingHandCursor);
+    boutonNouvellePartie->setGeometry(300, 100, 300, 70);
+
+    //Bouton pour charger une partie
+    QPushButton *boutonChargerPartie = new QPushButton("Charger une partie", &fenetreDemarrage);
+    boutonChargerPartie->setFont(maPolice);
+    boutonChargerPartie->setCursor(Qt::PointingHandCursor);
+    boutonChargerPartie->setGeometry(300, 200, 300, 70);
+
+    //Bouton pour quitter l'application
+    QPushButton *boutonQuitter = new QPushButton("Quitter", &fenetreDemarrage);
+    boutonQuitter->setFont(maPolice);
+    boutonQuitter->setCursor(Qt::PointingHandCursor);
+    boutonQuitter->setGeometry(300, 300, 300, 70);
+    QObject::connect(boutonQuitter, SIGNAL(clicked()), qApp, SLOT(quit()));
 
     fenetreDemarrage.show();
     return app.exec();
