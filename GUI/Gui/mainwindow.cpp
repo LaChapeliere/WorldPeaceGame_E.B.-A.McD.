5c0fe5b1f1c5d7c2611cc15e4e7ce9a4835f5b5c
Emma Barme <Emma@thomaslaptop.gateway.2wire.net>
2014-06-10 20:31:04 -0400

    Correction du code de la fenetre principal

--- GUI/Gui/mainwindow.cpp
+++ GUI/Gui/mainwindow.cpp
@@ -11,14 +11,15 @@ MainWindow::MainWindow(QWidget *parent) :
 
     //Barre de menus en haut
     menuBar()->setNativeMenuBar(false); //Sinon le menu ne s'affiche pas
-    QMenu *menuQuitter = menuBar()->addMenu("Quitter");
-
-    QMenu *menuNouvelle = menuBar()->addMenu("Nouvelle Partie");
-    QMenu *menuCharger = menuBar()->addMenu("Charger une partie");
-    QMenu *menuSauvegarder = menuBar()->addMenu("Sauvegarder la partie");
-
-    QAction *actionQuitter = new QAction("&Quitter", this);
-    menuQuitter->addAction(actionQuitter);
-    connect(actionQuitter, SIGNAL(triggered()), qApp, SLOT(quit()));
+    QMenu *menuFichier = menuBar()->addMenu("Fichier");
+    QAction *actionNouvellePartie = new QAction("Nouvelle Partie", this);
+    menuFichier->addAction(actionNouvellePartie);
+    QAction *actionSauverPartie = new QAction("Sauvegarder la partie", this);
+    menuFichier->addAction(actionSauverPartie);
+    QAction *actionChargerPartie = new QAction("Charger une partie", this);
+    menuFichier->addAction(actionChargerPartie);
+    QAction *actionQuitter = new QAction("Quitter", this);
+    menuFichier->addAction(actionQuitter);
+    QObject::connect(actionQuitter, SIGNAL(triggered()), qApp, SLOT(quit()));
 
 }
--- GUI/Gui/mainwindow.h
+++ GUI/Gui/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QApplication>
 #include <QLineEdit>
 #include <QFormLayout>
 #include <QMenuBar>
